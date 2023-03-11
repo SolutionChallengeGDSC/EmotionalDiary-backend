08a9f2bb8972012bb3f7f1dbc7ff4a51cd7e0dcd
EastWon0103 <dongwon000103@gmail.com>
2023-03-12 03:07:19 +0900

    add: findByEmail 명시

--- src/main/java/com/gdsc/EmotionalDiary/domain/user/UserRepository.java
+++ src/main/java/com/gdsc/EmotionalDiary/domain/user/UserRepository.java
@@ -2,6 +2,9 @@ package com.gdsc.EmotionalDiary.domain.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     User save(User user);
+    Optional<User> findByEmail(String email);
 }
