b029a17e2b07d0158a891f7868b13312dfd99f33
EastWon0103 <dongwon000103@gmail.com>
2023-03-12 03:07:47 +0900

    add: sign up 기능

--- src/main/java/com/gdsc/EmotionalDiary/controller/user/UserController.java
+++ src/main/java/com/gdsc/EmotionalDiary/controller/user/UserController.java
@@ -1,13 +1,17 @@
 package com.gdsc.EmotionalDiary.controller.user;
 
+import com.gdsc.EmotionalDiary.controller.user.dto.request.SignUpRequest;
 import com.gdsc.EmotionalDiary.response.CommonResponse;
 import com.gdsc.EmotionalDiary.response.ListResponse;
 import com.gdsc.EmotionalDiary.response.SingleResponse;
 import com.gdsc.EmotionalDiary.service.user.UserService;
+import com.gdsc.EmotionalDiary.service.user.dto.request.SignUpServiceRequest;
+import com.gdsc.EmotionalDiary.service.user.dto.response.SignUpSuccessResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,5 +21,20 @@ import java.util.List;
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
+    @PostMapping("/sign-up")
+    public final CommonResponse signUp(@RequestBody @Valid final SignUpRequest signUpRequest) {
+        logger.info("/user/sign-up 호출");
+        userService.duplicateUserEmail(signUpRequest.getEmail());
+        return SingleResponse.<SignUpSuccessResponse>builder()
+                .success(true)
+                .status(200)
+                .message("유저 DB 데이터 생성")
+                .result(
+                        userService.saveUser(SignUpServiceRequest.newInstance(
+                                signUpRequest.getEmail(),
+                                signUpRequest.getNickname())))
+                .build();
+    }
 }
