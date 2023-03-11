3390b965c438372086ed56d2b780f8d836c94d82
EastWon0103 <dongwon000103@gmail.com>
2023-03-12 03:09:48 +0900

    add: SignUp 로직 / 중복 유저 로직

--- src/main/java/com/gdsc/EmotionalDiary/service/user/UserService.java
+++ src/main/java/com/gdsc/EmotionalDiary/service/user/UserService.java
@@ -1,11 +1,39 @@
 package com.gdsc.EmotionalDiary.service.user;
 
+import com.gdsc.EmotionalDiary.domain.user.Role;
+import com.gdsc.EmotionalDiary.domain.user.User;
 import com.gdsc.EmotionalDiary.domain.user.UserRepository;
+import com.gdsc.EmotionalDiary.exception.DuplicateUserException;
+import com.gdsc.EmotionalDiary.service.user.dto.request.SignUpServiceRequest;
+import com.gdsc.EmotionalDiary.service.user.dto.response.SignUpSuccessResponse;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    public final SignUpSuccessResponse saveUser(final SignUpServiceRequest signUpServiceRequest) {
+        User successSaveUser = userRepository.save(User.newInstance(
+                signUpServiceRequest.getEmail(),
+                signUpServiceRequest.getNickname(),
+                Role.USER
+        ));
+
+        return SignUpSuccessResponse.of(
+                successSaveUser.getEmail(),
+                successSaveUser.getNickname()
+        );
+    }
+
+    public final void duplicateUserEmail(final String email) {
+        userRepository.findByEmail(email).ifPresent( user -> {
+                    logger.error("Duplicate User Exception을 발생");
+                    throw new DuplicateUserException("같은 이메일의 유저가 존재합니다.");
+        });
+    }
 }
