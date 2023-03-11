fa211b7f14a9894ae157654831e59ed349a3f60b
EastWon0103 <dongwon000103@gmail.com>
2023-03-12 03:10:55 +0900

    refactor: 줄바꿈 삭제

--- src/main/java/com/gdsc/EmotionalDiary/EmotionalDiaryApplication.java
+++ src/main/java/com/gdsc/EmotionalDiary/EmotionalDiaryApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EmotionalDiaryApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(EmotionalDiaryApplication.class, args);
 	}
