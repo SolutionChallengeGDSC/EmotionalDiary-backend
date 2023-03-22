b5b01c3025255919023de0133dd01e6af52f1e34
yuujinkim <66306573+yuujinkim@users.noreply.github.com>
2023-03-22 14:10:00 +0900

    feat: diary domain

---
+++ src/main/java/com/gdsc/EmotionalDiary/domain/diary/Diary.java
@@ -0,0 +1,61 @@
+package com.gdsc.EmotionalDiary.domain.diary;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.gdsc.EmotionalDiary.domain.BaseTimeEntity;
+import com.gdsc.EmotionalDiary.domain.user.User;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "DIARY")
+public class Diary extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Long id;
+
+    @NotNull
+    @Column(name = "TITLE", length = 28)
+    private String title;
+
+    @NotNull
+    @Column(name = "CONTENT")
+    private String content;
+
+    @NotNull
+    @Column(name = "ISPRIVATE")
+    private boolean isprivate;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "USER_ID")
+    @JsonIgnore
+    private User user;
+
+    private Diary(String title, String content, User user) {
+        this.title = title;
+        this.content = content;
+        this.isprivate = false;
+        this.user = user;
+    }
+
+    public static final Diary newInstance(String title, String content, User user) {
+        return new Diary(title, content, user);
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setContent(String content) { this.content = content; }
+
+    public void setIsprivate(boolean isprivate) {
+        this.isprivate = isprivate;
+    }
+}
